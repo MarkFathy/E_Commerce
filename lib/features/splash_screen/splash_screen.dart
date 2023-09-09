8e5bf8e0d175d21a3f74e3d2fe9b81671a41704f
MarkFathy <markfathy192@gmail.com>
2023-09-09 15:47:34 +0300

    update

---
+++ lib/features/splash_screen/splash_screen.dart
@@ -0,0 +1,28 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:e_commerce/features/presentation/onbording/onboarding.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+
+      duration: 2000,
+        splash: Image(
+          height: 100.h,
+            width: 100.w,
+            image:const AssetImage('assets/images/splash1.png')),
+        splashTransition: SplashTransition.rotationTransition,
+        nextScreen:const OnBoarding());
+
+  }
+}
+
+// SpinKitThreeBounce(
+// color: firstColor,
+// size: 30.0,
+// ),
